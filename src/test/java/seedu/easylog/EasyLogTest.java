67694f7e4d76262c9f3db66e778380e6ebe19d32
Zhu Zikun @ NUS <zhu.zikun@u.nus.edu>
2021-03-10 17:48:16 +0800

    Add JUnit Testing: JUnit tests
    Zikun adds a JUnit test for EasyLog.java.

--- src/test/java/seedu/easylog/EasyLogTest.java
+++ src/test/java/seedu/easylog/EasyLogTest.java
@@ -1,12 +1,23 @@
 package seedu.easylog;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class EasyLogTest {
+
     @Test
     public void sampleTest() {
         assertTrue(true);
     }
+
+    @Test
+    @DisplayName("Should Print the Greeting Message")
+    public void easyLog_shouldPrintGreetingMessage() {
+        assertEquals("Hello! I'm easyLog!\\n\" + \"What can I do for you? Enter help to view commands.",
+                "Hello! I'm easyLog!\\n\" + \"What can I do for you? Enter help to view commands.");
+    }
+
 }

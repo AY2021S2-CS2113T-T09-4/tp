e948738b8e8d413b1f73ffa8a6523ac2a6592d5b
Yiwen_Qiu <60378963+e00426142@users.noreply.github.com>
2021-03-10 18:31:13 +0800

    jUnitTest completed

--- src/main/java/seedu/easylog/order/Order.java
+++ src/main/java/seedu/easylog/order/Order.java
@@ -1,4 +1,13 @@
 package seedu.easylog.order;
 
 public class Order {
+    protected String orderName;
+
+    public Order(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
 }
--- src/test/java/seedu/easylog/order/OrderTest.java
+++ src/test/java/seedu/easylog/order/OrderTest.java
@@ -1,4 +1,13 @@
 package seedu.easylog.order;
 
+import org.junit.jupiter.api.Test;
+import seedu.easylog.item.Item;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class OrderTest {
+    @Test
+    public void testGetOrderName() {
+        assertEquals("MP3", new Order("MP3").getOrderName());
+    }
 }
